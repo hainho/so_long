34361a48944f67b445adb4354241252324e709c6
hainho <hainho9704@gmail.com>
2022-02-26 04:35:33 +0900

    add : init mlx, win

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: iha <iha@student.42.kr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 02:51:52 by iha               #+#    #+#             */
-/*   Updated: 2022/02/26 02:51:52 by iha              ###   ########.fr       */
+/*   Updated: 2022/02/26 04:29:44 by iha              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,7 @@ void	init(t_solong *sl)
 	sl->map_info.e_num = 0;
 	sl->player_info.move = 0;
 	sl->map_info.map = NULL;
+	sl->mlx_ptr = NULL;
+	sl->win_ptr = NULL;
 	return ;
 }
